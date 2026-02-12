c8b7b60766cfbbd98ee150eb32a9b425267b89e8
ClaudIA <claudia.llbcorp@gmail.com>
2026-02-12 06:57:53 +0100

    fix: remove unused imports from utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,6 @@
 import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
-import { resolveOAuthDir } from "./config/paths.js";
-import { logVerbose, shouldLogVerbose } from "./globals.js";
 import {
   expandHomePrefix,
   resolveEffectiveHomeDir,
